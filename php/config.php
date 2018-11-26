0f01dc93cc8fab2a07991ef49163ecd604208b89
matheusjohannaraujo <matheusjohannaraujo@gmail.com>
2018-11-26 09:42:21 -0300

    Atualização

--- php/config.php
+++ php/config.php
@@ -157,7 +157,7 @@ function abrirArquivo($id){
         $fp = fopen($dir . $nome, "a+"); 
         if (flock($fp, LOCK_EX)) {
             fclose($fp);
-            sleep(25);
+            sleep(20);
             unlink($dir . $nome);
         } else {
             fclose($fp);
@@ -202,7 +202,7 @@ function baixarArquivo($id){
         $fp = fopen($dir . $nome, "a+"); 
         if (flock($fp, LOCK_EX)) {
             fclose($fp);
-            sleep(25);
+            sleep(20);
             unlink($dir . $nome);
         } else {
             fclose($fp);
